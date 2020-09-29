519e124e178c944e3a1df21217b8b633727ec939
alex@awithsmith.com <alex@awithsmith.com>
2020-09-29 08:59:37 +0100

    added operate function

--- main.js
+++ main.js
@@ -3,3 +3,5 @@ const add = (x, y) => { return x + y };
 const subtract = (x, y) => { return x - y };
 const multiply = (x, y) => { return x * y };
 const divide = (x, y) => { return x / y };
+
+const operate = (operator, x, y) => { return operator(x, y) };
