3ab4b43670899ec62a08660bb210f9bb8ea051e3
alex@awithsmith.com <alex@awithsmith.com>
2020-09-29 08:57:22 +0100

    basic calculation functions

---
+++ main.js
@@ -0,0 +1,5 @@
+// FUNCTIONS
+const add = (x, y) => { return x + y };
+const subtract = (x, y) => { return x - y };
+const multiply = (x, y) => { return x * y };
+const divide = (x, y) => { return x / y };
