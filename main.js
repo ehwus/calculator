67bd3609fff37dab2ae72ac3de40a34cfe5a14a5
Alex Withington-Smith <alex@awithsmith.com>
2020-09-30 13:02:40 +0100

    added event listener to buttons

--- main.js
+++ main.js
@@ -3,5 +3,12 @@ const add = (x, y) => { return x + y };
 const subtract = (x, y) => { return x - y };
 const multiply = (x, y) => { return x * y };
 const divide = (x, y) => { return x / y };
-
 const operate = (operator, x, y) => { return operator(x, y) };
+
+// add listeners to buttons
+const buttons = document.querySelectorAll('.buttons button')
+for (const button of buttons) {
+    button.addEventListener("click", function () {
+        console.log(button.id)
+    })
+} 
