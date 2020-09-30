6a3de3f96b67afd3d31cd025ec246d90be4c9992
Alex Withington-Smith <alex@awithsmith.com>
2020-09-30 13:17:41 +0100

    got the display to update with single number on button press

--- main.js
+++ main.js
@@ -5,14 +5,25 @@ const multiply = (x, y) => { return x * y };
 const divide = (x, y) => { return x / y };
 const operate = (operator, x, y) => { return operator(x, y) };
 
+// CONSTANTS
+const display = document.querySelector(".display");
+
+// VARIABLES
+let displayNumber;
+
 // logic to handle button press
 function handleButton(button) {
     if (button.id >= 0 && button.id < 10) {
+        displayNumber = button.id
         console.log(`Number ${button.id}`)
     } else {
         console.log(button.id)
     }
-    
+    updateDisplay()
+}
+// update display using the global displayNumber variable
+function updateDisplay() {
+    display.innerHTML = displayNumber
 }
 
 // add listeners to buttons
