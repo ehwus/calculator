74dc415f41642a86ddf17d5e832333780caed910
Alex Withington-Smith <alex@awithsmith.com>
2020-09-30 13:11:31 +0100

    added logic to tell if button pressed was number or operator

--- main.js
+++ main.js
@@ -5,10 +5,20 @@ const multiply = (x, y) => { return x * y };
 const divide = (x, y) => { return x / y };
 const operate = (operator, x, y) => { return operator(x, y) };
 
+// logic to handle button press
+function handleButton(button) {
+    if (button.id >= 0 && button.id < 10) {
+        console.log(`Number ${button.id}`)
+    } else {
+        console.log(button.id)
+    }
+    
+}
+
 // add listeners to buttons
 const buttons = document.querySelectorAll('.buttons button')
 for (const button of buttons) {
     button.addEventListener("click", function () {
-        console.log(button.id)
+        handleButton(button)
     })
 } 
